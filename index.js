7c9328658e8edb47f06a3641f424700e0f4a2b26
Victor Hugo <135546677+vickkj@users.noreply.github.com>
2025-04-10 10:56:05 -0300

    aula 10-04

---
+++ index.js
@@ -0,0 +1,20 @@
+//require('dotenv').config();
+const express = require('express'); 
+const cors = require('cors');
+
+//const router = require('./src/routes/routes'); 
+
+const app = express(); 
+app.use(cors()); 
+app.use(express.json()); 
+//app.use(router);
+
+const porta = process.env.PORT || 3333;
+
+app.listen(porta, () => {
+    console.log(`Servidor iniciado em http://localhost:${porta}`);
+});
+
+app.get('/', (request, response) => {
+    response.send('Documentação da API');
+});
