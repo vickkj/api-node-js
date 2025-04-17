7e86f6fe6fe83ad25819b948f274d1bc74873ff4
Victor Hugo <135546677+vickkj@users.noreply.github.com>
2025-04-17 10:26:09 -0300

    17-04

---
+++ src/controllers/usuarios.js
@@ -0,0 +1,64 @@
+const db = require('../dataBase/connection'); 
+
+module.exports = {
+    async listarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Lista de usuários', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    }, 
+    async cadastrarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Cadastro de usuários', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    }, 
+    async editarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Alteração no cadastro de usuário', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    }, 
+    async apagarUsuarios(request, response) {
+        try {
+            return response.status(200).json({
+                sucesso: true, 
+                mensagem: 'Exclusão de usuário', 
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false, 
+                mensagem: 'Erro na requisição.', 
+                dados: error.message
+            });
+        }
+    }, 
+};  
---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+const express = require('express'); 
+const router = express.Router(); 
+
+const UsuariosController = require('../controllers/usuarios'); 
+
+router.get('/usuarios', UsuariosController.listarUsuarios); 
+router.post('/usuarios', UsuariosController.cadastrarUsuarios); 
+router.patch('/usuarios', UsuariosController.editarUsuarios); 
+router.delete('/usuarios', UsuariosController.apagarUsuarios); 
+
+
+module.exports = router;
