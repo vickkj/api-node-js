73755a24340d76150ee0922e68ffff5ab121f01e
Victor Hugo <135546677+vickkj@users.noreply.github.com>
2025-06-05 10:52:15 -0300

    08

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,6 @@ router.get('/feedback-consulta', FeedbackConsulta.listarFeedback_consulta);
 router.post('/feedback-consulta', FeedbackConsulta.cadastrarFeedback_consulta);
 router.patch('/feedback-consulta/:fdbk_id', FeedbackConsulta.editarFeedback_consulta);
 router.delete('/feedback-consulta/:fdbk_id', FeedbackConsulta.apagarFeedback_consulta);
-router.delete('/feedback-consulta/:fdbk_id', FeedbackConsulta.ocultarFeedback_consulta);
+router.delete('/feedback-consulta/del/:fdbk_id', FeedbackConsulta.ocultarFeedback_consulta);
 
 module.exports = router;
